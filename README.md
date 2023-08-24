a3cae8171d17def57dde38667f712106230429a6
7799043 <leoniak.maciek1@gmail.com>
2023-08-24 21:50:32 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-This is my RestAPI playground 
+### Goal
+For this repo, I have two main goals. The first is to improve my knowledge about API testing. The second is to have my first hands-on experience with TypeScript.
+
+### Tools  
+For my API testing I am using Jest framework.
+
+### Report 
+
+![Screenshot][def]
+
+[def]: test-report/screenshots/ex.test.png
--- tests/ex.test.ts
+++ tests/ex.test.ts
@@ -3,16 +3,16 @@ import axios, { AxiosError } from 'axios';
 describe('API Tests', () => {
   it('should fetch data from the API and validate user IDs', async () => {
     const url = 'https://gorest.co.in/public/v2/users'; // API URL
-    
+
     try {
       const response = await axios.get(url);
-      
-       // Log the API response data to the console
-       console.log('API Response Data:', response.data);
+
+      // Log the API response data&status to the console
+      console.log('API Response Data:', response.data);
+      console.log('API Response Status:', response.status);
 
       // Assertions
-      expect(response.status).toBe(200); // Check response status
-
+      expect(response.status).toBe(500); // Check response status
       const users = response.data.data; // Assuming the user objects are in the 'data' property
       expect(Array.isArray(users)).toBe(true); // Check if users is an array
 
