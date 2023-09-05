61412c624d6f1f368f484f345403915bbaca1a27
7799043 <leoniak.maciek1@gmail.com>
2023-09-05 20:24:38 +0200

    .

--- tests/delete.test.ts
+++ tests/delete.test.ts
@@ -7,10 +7,35 @@ describe('API CRUD Tests', () => {
     const url = 'https://gorest.co.in/public/v2/users'; // API URL
 
     it('should create a new user', async () => {
-        // ... (Your code for creating a new user)
 
-        // Your existing test for creating a new user goes here
+        const postData = createRandomUser();
 
+        const headers = {
+            Authorization: `Bearer ${process.env.AUTH_TOKEN}`
+        };
+
+        try {
+
+            const response = await axios.post(url, postData, { headers });
+
+            console.log(response.data);
+            console.log(response.status);
+
+
+            expect(response.status).toBe(201);
+            expect(response.data).toMatchObject(postData); // Assuming the API returns the created user
+
+
+        } catch (error: any) {
+            if (axios.isAxiosError(error)) {
+                // Handle AxiosError
+                // You can extract information from the error object here
+                const errorMessage = `Request failed: ${error.message}`;
+                throw new Error(errorMessage);
+            }
+            // Handle other types of errors
+            throw error; // Rethrow the original error if not an AxiosError
+        }
     });
 
     it('should delete an existing user', async () => {
