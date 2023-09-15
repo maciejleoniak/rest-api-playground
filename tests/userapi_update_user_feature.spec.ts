b5fe98dc1894a7d7e0c53e2d9d90b5d8eba9d90b
7799043 <leoniak.maciek1@gmail.com>
2023-09-15 13:44:16 +0200

    user atribute update

--- tests/userapi_update_user_feature.spec.ts
+++ tests/userapi_update_user_feature.spec.ts
@@ -1,57 +1,64 @@
 import axios from 'axios';
-import { createRandomUser, getRandomEmail, getRandomName } from './data/randomUser';
-
+import { createRandomUser, getRandomEmail } from './data/randomUser';
 
 require('dotenv').config();
 
 describe('API CRUD Tests - update user', () => {
     const url = 'https://gorest.co.in/public/v2/users'; // API base URL
+    const createHeaders = {
+        Authorization: `Bearer ${process.env.AUTH_TOKEN}`
+    };
+    let userId: number; // Store the ID of the created user
 
-    it('should update an existing user', async () => {
-        // Prepare data for creating a new user
+    beforeAll(async () => {
+        // Create a user before any test case
         const createData = createRandomUser();
+        const createResponse = await axios.post<any>(url, createData, { headers: createHeaders });
+        expect(createResponse.status).toBe(201);
+        userId = createResponse.data.id; // Store the ID for later use
+    });
 
-        const createHeaders = {
+    it('should update an existing user email', async () => {
+        const emailUpdate = getRandomEmail();
+
+        // Prepare data for updating the user
+        const updateData = {
+            email: emailUpdate
+        };
+
+        const updateHeaders = {
             Authorization: `Bearer ${process.env.AUTH_TOKEN}`
         };
 
-        try {
-            // Create a user
-            const createResponse = await axios.post(url, createData, { headers: createHeaders });
-            expect(createResponse.status).toBe(201);
+        // Update the user
+        const updateResponse = await axios.put<any>(`${url}/${userId}`, updateData, { headers: updateHeaders });
+        expect(updateResponse.status).toBe(200);
 
-            // Get the ID of the newly created user
-            const userId = createResponse.data.id;
+        // Check if the data was updated as expected
+        expect(updateResponse.data).toMatchObject(updateData);
+    });
 
-            const nameUpdate = getRandomName();
-            const emailUpdate = getRandomEmail();
+    it('should update an existing user status', async () => {
+        // Retrieve the initial status of the user
+        const initialStatus = 'active'; // Assuming it's initially active
 
-            // Prepare data for updating the user
-            const updateData = {
-                name: nameUpdate,
-                email: emailUpdate
+        // Calculate the new status (opposite of the initial status)
+        const newStatus = initialStatus === 'active' ? 'inactive' : 'active';
 
-            };
+        // Prepare data for updating the user's status
+        const updateData = {
+            status: newStatus
+        };
 
-            const updateHeaders = {
-                Authorization: `Bearer ${process.env.AUTH_TOKEN}`
-            };
+        const updateHeaders = {
+            Authorization: `Bearer ${process.env.AUTH_TOKEN}`
+        };
 
-            // Update the user
-            const updateResponse = await axios.put(`${url}/${userId}`, updateData, { headers: updateHeaders });
-            expect(updateResponse.status).toBe(200);
+        // Update the user
+        const updateResponse = await axios.put<any>(`${url}/${userId}`, updateData, { headers: updateHeaders });
+        expect(updateResponse.status).toBe(200);
 
-            // Check if the data was updated as expected
-            expect(updateResponse.data).toMatchObject(updateData);
-
-        } catch (error: any) {
-            if (axios.isAxiosError(error)) {
-                // Handle Axios errors
-                const errorMessage = `Request failed: ${error.message}`;
-                throw new Error(errorMessage);
-            }
-            // Handle other types of errors
-            throw error; // Throw the original error if it's not an Axios error
-        }
+        // Check if the data was updated as expected
+        expect(updateResponse.data.status).toBe(newStatus);
     });
 });
